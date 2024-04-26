27b5685ad5c012199670ede4005cca460510b991
parimalgame <168087540+parimalgame@users.noreply.github.com>
2024-04-26 16:22:40 +0530

    home screen design
    create home screen and add layout selection option

---
+++ My_First_Game/Assets/Script/HomeManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnLayoutSelection(int index)
+    {
+        PlayerPrefs.SetInt("layout", index);
+        SceneManager.LoadScene("Game");
+    }
+}
